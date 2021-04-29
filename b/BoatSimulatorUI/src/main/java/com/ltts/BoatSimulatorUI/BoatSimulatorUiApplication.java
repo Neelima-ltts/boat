abfb3f90d9065d4e7452c917638bd110e26310f4
Neelima-takasi <neelimanaidu.takasi@ltts.com>
2021-04-29 16:34:40 +0530

    commit

---
+++ b/BoatSimulatorUI/src/main/java/com/ltts/BoatSimulatorUI/BoatSimulatorUiApplication.java
@@ -0,0 +1,13 @@
+package com.ltts.BoatSimulatorUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatSimulatorUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatSimulatorUiApplication.class, args);
+	}
+
+}
---
+++ b/BoatSimulatorUI/src/test/java/com/ltts/BoatSimulatorUI/BoatSimulatorUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltts.BoatSimulatorUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatSimulatorUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
